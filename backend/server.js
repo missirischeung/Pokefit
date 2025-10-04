9577d72c1676fd6e5882def729c1796f546173d8
Jeff <jho1211@students.cs.ubc.ca>
2025-10-04 12:56:23 -0700

    Add database driver files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import sql from './db.js';
+import express from 'express';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express();
+const port = 3000;
+
+
+async function getTrainersOver(age) {
+    const users = await sql`
+        SELECT *
+        FROM Trainer
+        WHERE AGE > ${ age }
+    `;
+
+    console.log(users);
+
+    return users;
+}
+
+app.get('/', async (req, res) => {
+    await getTrainersOver(0);
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
