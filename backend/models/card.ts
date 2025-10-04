c128cf2c658521c6e8314bc35c3e0346c4e1aca7
Jeff <jho1211@students.cs.ubc.ca>
2025-10-04 15:27:46 -0700

    Added a qty query var for /cards/:setId endpoint

--- backend/models/card.ts
+++ backend/models/card.ts
@@ -10,14 +10,34 @@ export type Card = {
     image: string;
 };
 
-function getCards(set: string): Card[] {
+function getCardSet(set: string): Card[] {
     const data = fs.readFileSync(cardPath, "utf8");
     const allCards = JSON.parse(data) as Card[];
     return allCards.filter(card => card.setId === set);
 }
 
+export function drawCardsFromSet(set: string, count: number): Card[] {
+    const cards = setMap[set] ?? [];
+    const drawnCards: Card[] = [];
+
+    if (cards.length === 0) return [];
+
+    let i = 0;
+    while (i < count && drawnCards.length < cards.length) {
+        const randomIndex = Math.floor(Math.random() * cards.length);
+        const randomCard = cards[randomIndex]!;
+
+        if (!drawnCards.includes(randomCard)) {
+            drawnCards.push(randomCard);
+            i++;
+        }
+    }
+    
+    return drawnCards;
+}
+
 const setMap: { [key: string]: Card[] } = {
-    "sv3pt5": getCards("sv3pt5"),
+    "sv3pt5": getCardSet("sv3pt5"),
 }
 
 export const setCards = setMap;
